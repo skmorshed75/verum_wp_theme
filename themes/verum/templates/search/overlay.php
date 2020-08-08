085f5172cb460f6e05ffab306cc408de6c8effda
skmorshed75 <skmorshed75@yahoo.com>
2020-08-08 07:53:23 +0600

    ৪.৩৪ - সার্চ ফর্মে লেটেস্ট পোস্ট সেকশনটা দেখানো

--- themes/verum/templates/search/overlay.php
+++ themes/verum/templates/search/overlay.php
@@ -8,81 +8,58 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h4 class="text-light search-blog-title">Latest Posts</h4>
+                    <h4 class="text-light search-blog-title">
+                        <?php _e("Latest Posts","verum"); ?>
+                    </h4>
                 </div>
             </div>
         </div>
         <div class="search-blog-post">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-3">
-                        <article class="post">
-                            <div class="post-img">
-                                <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/b2.jpg" alt=""></a>
-                            </div>
-                            <div class="post-header">
-                                <h3><a href="#">Alicia Keys is on the Picturesque Trip of a Lifetime in Egypt</a></h3>
-                                <div class="post-meta">
-                                    <ul class="cat">
-                                        <li><a href="#">Movie</a></li>
-                                    </ul>
-                                    <div class="separation"></div>
-                                    <div class="post-date"><a href="#">28th June 2018</a></div>
+                    <?php
+                    $verum_latest_posts_args = array(
+                        'posts_per_page' => 4,
+                        'post_type' => 'post',
+                        'post_status' => 'publish',
+                        'ignore_sticky_post' => 1
+                    );
+                    $verum_latest_posts = new WP_Query($verum_latest_posts_args);
+
+                    while($verum_latest_posts->have_posts()):
+                        $verum_latest_posts->the_post();
+                        ?>
+                        <div class="col-md-3">
+                            <article class="post">
+                                <div class="post-img">
+                                    <?php the_post_thumbnail('medium'); ?>
                                 </div>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-md-3">
-                        <article class="post">
-                            <div class="post-img">
-                                <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/b3.jpg" alt=""></a>
-                            </div>
-                            <div class="post-header">
-                                <h3><a href="#">Alicia Keys is on the Picturesque Trip of a Lifetime in Egypt</a></h3>
-                                <div class="post-meta">
-                                    <ul class="cat">
-                                        <li><a href="#">Movie</a></li>
-                                    </ul>
-                                    <div class="separation"></div>
-                                    <div class="post-date"><a href="#">28th June 2018</a></div>
+                                <div class="post-header">
+                                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                    <div class="post-meta">
+                                        <?php
+                                        $verum_categories = get_the_category(); 
+                                        if($verum_categories) :
+                                            echo '<ul class="cat">';
+                                            foreach($verum_categories as $verum_category) :
+                                                printf('<li><a href="%s">%s</a></li>',get_category_link($verum_category),$verum_category->name);
+                                                break; //show only one category
+                                            endforeach;
+                                            echo '</ul>';
+                                            ?>
+                                            <div class="separation"></div>
+                                            <?php
+                                        endif;
+                                        ?>
+                                        <div class="post-date"><a href="<?php the_permalink(); ?>"><?php echo get_the_date("j F Y"); ?></a></div>
+                                    </div>
                                 </div>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-md-3">
-                        <article class="post">
-                            <div class="post-img">
-                                <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/b4.jpg" alt=""></a>
-                            </div>
-                            <div class="post-header">
-                                <h3><a href="#">Alicia Keys is on the Picturesque Trip of a Lifetime in Egypt</a></h3>
-                                <div class="post-meta">
-                                    <ul class="cat">
-                                        <li><a href="#">Movie</a></li>
-                                    </ul>
-                                    <div class="separation"></div>
-                                    <div class="post-date"><a href="#">28th June 2018</a></div>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-md-3">
-                        <article class="post">
-                            <div class="post-img">
-                                <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/img/b5.jpg" alt=""></a>
-                            </div>
-                            <div class="post-header">
-                                <h3><a href="#">Alicia Keys is on the Picturesque Trip of a Lifetime in Egypt</a></h3>
-                                <div class="post-meta">
-                                    <ul class="cat">
-                                        <li><a href="#">Movie</a></li>
-                                    </ul>
-                                    <div class="separation"></div>
-                                    <div class="post-date"><a href="#">28th June 2018</a></div>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
+                            </article>
+                        </div>
+                    <?php
+                    endwhile;
+                    wp_reset_query();
+                    ?>
                 </div>
             </div>
         </div>
