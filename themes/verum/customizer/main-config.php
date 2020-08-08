21b5d137c3a6f5c0a6e97f92e10e16dee0ab9713
skmorshed75 <skmorshed75@yahoo.com>
2020-08-08 18:19:05 +0600

    ৪.৩৫ - কাস্টোমাইজার এবং কিরকি রিপিটার ফিল্ডের সাহায্যে সার্চ সেকশনে ফিচার্ড পোস্ট দেখানো (চ্যালেঞ্জিং)

--- themes/verum/customizer/main-config.php
+++ themes/verum/customizer/main-config.php
@@ -1,22 +1,22 @@
 <?php
-if(class_exists('Kirki')) {
+if ( class_exists( 'Kirki' ) ) {
     Kirki::add_config( 'verum_options', array(
-        'capability'    => 'edit_theme_options',
-        'option_type'   => 'theme_mod',
+        'capability'  => 'edit_theme_options',
+        'option_type' => 'theme_mod',
     ) );
 
     //Class 4.20 Create Panel
     Kirki::add_panel( 'verum_panel', array(
-        'priority'    => 10,
-        'title'       => esc_html__( 'Verum Options', 'verum' ),
+        'priority' => 10,
+        'title'    => esc_html__( 'Verum Options', 'verum' ),
     ) );
 
     //Create Section
     Kirki::add_section( 'blog_settings', array(
-        'title'          => esc_html__( 'Blog Settings', 'verum' ),
+        'title'    => esc_html__( 'Blog Settings', 'verum' ),
         //'description'    => esc_html__( 'My section description.', 'kirki' ),
-        'panel'          => 'verum_panel',
-        'priority'       => 160,
+        'panel'    => 'verum_panel',
+        'priority' => 160,
     ) );
 
     //Add a Control - SELECT
@@ -30,9 +30,72 @@ if(class_exists('Kirki')) {
         'priority'    => 10,
         'multiple'    => 1,
         'choices'     => [
-            'no' => esc_html__( 'No Sidebar', 'verum' ),
-            'left' => esc_html__( 'Left Sidebar', 'verum' ),
+            'no'    => esc_html__( 'No Sidebar', 'verum' ),
+            'left'  => esc_html__( 'Left Sidebar', 'verum' ),
             'right' => esc_html__( 'Right Sidebar', 'verum' ),
         ],
     ] );
+
+    //Class 4.35
+    Kirki::add_section( 'search_settings', array(
+        'title'    => esc_html__( "Search Settings", "verum" ),
+        'panel'    => 'verum_panel',
+        'priority' => 160,
+    ) );
+
+    Kirki::add_field( 'verum_options', [
+        'type'     => 'text',
+        'settings' => 'search_heading',
+        'label'    => esc_html__( 'Heading', 'verum' ),
+        'section'  => 'search_settings',
+        'default'  => 'Latest Posts',
+        'priority' => 10,
+    ] );
+
+    Kirki::add_field( 'verum_options', [
+        'type'     => 'select',
+        'settings' => 'search_post_source',
+        'label'    => esc_html__( 'Post Source', 'verum' ),
+        'section'  => 'search_settings',
+        'default'  => 'latest',
+        'priority' => 10,
+        'choices'  => [
+            'latest'   => esc_html__( 'Latest Posts', 'verum' ),
+            'featured' => esc_html__( 'Featured Posts', 'verum' ),
+        ],
+    ] );
+
+    Kirki::add_field( 'verum_options', [
+        'type'            => 'repeater',
+        'label'           => esc_html__( 'Featured Post', 'kirki' ),
+        'section'         => 'search_settings',
+        'priority'        => 10,
+        'row_label'       => [
+            'type'  => 'field',
+            'value' => esc_html__( 'Featured Post', 'kirki' ),
+            'field' => 'post',
+        ],
+        'button_label'    => esc_html__( 'Add new ', 'kirki' ),
+        'settings'        => 'search_featured_posts',
+
+        'fields'          => [
+            'post' => [
+                'type'    => 'select',
+                'label'   => esc_html__( 'Featured Post', 'kirki' ),
+                'choices' => Kirki_helper::get_posts( array(
+                    'posts_per_page' => -1,
+                    'post_type'      => 'post',
+                    'post_status'    => 'publish',
+                ) ),
+            ],
+        ],
+        'active_callback' => array(
+            array(
+                'setting'  => 'search_post_source',
+                'value'    => 'featured',
+                'operator' => '==',
+            ),
+        ),
+    ] );
+
 }
--- themes/verum/templates/search/overlay.php
+++ themes/verum/templates/search/overlay.php
@@ -1,5 +1,10 @@
 
 <!--search overlay start-->
+<?php
+$verum_post_source = get_theme_mod("search_post_source","latest");
+$verum_search_heading = get_theme_mod("search_heading",__("Latest Posts","verum"));
+
+?>
 <div class="search-wrap">
     <div class="overlay">
 
@@ -9,7 +14,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <h4 class="text-light search-blog-title">
-                        <?php _e("Latest Posts","verum"); ?>
+                        <?php echo esc_html($verum_search_heading); ?>
                     </h4>
                 </div>
             </div>
@@ -18,6 +23,9 @@
             <div class="container">
                 <div class="row">
                     <?php
+                    
+                if("latest" == $verum_post_source) :
+                    //LATEST POST
                     $verum_latest_posts_args = array(
                         'posts_per_page' => 4,
                         'post_type' => 'post',
@@ -59,6 +67,54 @@
                     <?php
                     endwhile;
                     wp_reset_query();
+                else :
+                    //FEATURED POSTS
+                    $_verum_featured_posts_id = get_theme_mod("search_featured_posts");
+                    $verum_featured_posts_id = array_column($_verum_featured_posts_id,'post');
+                    $verum_featured_posts_args = array(
+                        // 'posts_per_page' => 4,
+                        'post_type' => 'post',
+                        'post_status' => 'publish',
+                        'ignore_sticky_post' => 1,
+                        'post__in' => $verum_featured_posts_id,
+                        'orderby' => 'post__in'
+                    );
+                    $verum_featured_posts = new WP_Query($verum_featured_posts_args);
+
+                    while($verum_featured_posts->have_posts()):
+                        $verum_featured_posts->the_post();
+                        ?>
+                        <div class="col-md-3">
+                            <article class="post">
+                                <div class="post-img">
+                                    <?php the_post_thumbnail('medium'); ?>
+                                </div>
+                                <div class="post-header">
+                                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                    <div class="post-meta">
+                                        <?php
+                                        $verum_categories = get_the_category(); 
+                                        if($verum_categories) :
+                                            echo '<ul class="cat">';
+                                            foreach($verum_categories as $verum_category) :
+                                                printf('<li><a href="%s">%s</a></li>',get_category_link($verum_category),$verum_category->name);
+                                                break; //show only one category
+                                            endforeach;
+                                            echo '</ul>';
+                                            ?>
+                                            <div class="separation"></div>
+                                            <?php
+                                        endif;
+                                        ?>
+                                        <div class="post-date"><a href="<?php the_permalink(); ?>"><?php echo get_the_date("j F Y"); ?></a></div>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    <?php
+                    endwhile;
+                    wp_reset_query();
+                endif;
                     ?>
                 </div>
             </div>
